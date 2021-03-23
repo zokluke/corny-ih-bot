9e5793bdb4e7c24f041a32dc82fbf6eb0d042e12
Luke <68570779+zokluke@users.noreply.github.com>
2021-03-23 01:38:08 -0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -143,13 +143,15 @@ async def delrole(ctx, *, rolename):
     await ctx.channel.send("that role doesnt exist dummy!!")
 
 @client.command()
+async def attic(ctx):
+	await ctx.channel.send("https://cdn.discordapp.com/attachments/805610014814240818/823805119946686514/image0.png")
+
+@client.command()
 @has_permissions(manage_messages=True)
 async def unban(ctx, *, member):
 	banned_users = await ctx.guild.bans()
 	
-@client.command()
-async def attic(ctx):
-  await ctx.channel.send("https://cdn.discordapp.com/attachments/805610014814240818/823805119946686514/image0.png")
+
 
 	for ban_entry in banned_users:
 		user = ban_entry.user
